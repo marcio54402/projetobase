6f5196df4b0116a79ae84160345b0329c5e6ef70
marcio54402 <mvictoor11@hotmail.com>
2025-06-03 11:33:52 -0300

    ninhodeif

--- logicacomjs.js
+++ logicacomjs.js
@@ -1,1 +1,55 @@
-console.log("OI")
+/*let idade= prompt ("Digite aqui sua idade:")
+if (idade < 12) {
+alert ("Assista conteúdo infantil") 
+}
+else if (idade >=12 && idade<=17){
+alert ("Assista conteúdo teen")
+}
+else if (idade>=18 && idade<=60){
+alert ("Assista conteúdo adulto")
+}
+else {
+    alert ("Assista conteúdo sênior")
+}
+let *
+pontuacao= prompt ("Digite aqui sua pontuação")
+if (pontuacao <= 1000) 
+    alert (" Seu nivel é de aprendiz e seu bõ[ônus de xp é 200")
+        else if (pontuacao >=1001 && pontuacao<=5000){
+    alert (" Seu nível é de aprendiz e seu bônus de xp é 500")
+}
+else if (pontuacao >=5001 && pontuacao <=10000){
+alert (" Seu nível é de expert e seu bônus de xp é de 1000")
+}
+else {
+    alert (" Seu nível é de mestre e seu bônus de xp é de 2000")
+}*/
+
+
+let valorbasico= 25;
+let valorpremium = 40;
+let valorultra = 60;
+let tipoassinatura = prompt("Digite a sua assinatura")
+let quantitelas = prompt("Digite a quantidade de telas que deseja")
+if (tipoassinatura == "básico")
+    if(quantitelas >=1 && quantitelas <=2){
+alert("Seu plano terá o valor de "+valorbasico+" R$")
+    }
+    else if (quantitelas >=3 && quantitelas <=4){
+        alert("Seu plano terá o valor de R$"+(valorbasico+telasmaisde4))
+        
+        }
+        else{
+
+        }
+        else if (tipoassinatura == "premium"){
+
+        }
+
+
+
+
+
+
+
+
